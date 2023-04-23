4457d36ba859a6e238cc9db1a6eb5406c08ba856
thesaint <daniel_dimitrov77@icloud.com>
2023-04-23 10:43:11 +0300

    Rest API with books

---
+++ WebAPI_andREST_controllers/books-rest/src/main/java/bg/softuni/booksrest/BooksRestApplication.java
@@ -0,0 +1,13 @@
+package bg.softuni.booksrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksRestApplication.class, args);
+	}
+
+}
